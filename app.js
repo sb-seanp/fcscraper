690c776aaf1684c1def97a8833e956ab5f21a9b2
Sean Prasertsit <sean@prasertsit.com>
2015-04-23 01:46:49 -0700

    Finished scraper. TODO updater

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
  * Created by Sean on 3/21/2015.
  */
 
-var scraper = require('./lib/scrapers/tableScraper');
+var scraper = require('./lib/scrapers/clubScraper');
 
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    scraper.scrape('http://www.livescore.co.uk/soccer/england/premier-league/');
+    scraper.scrape('http://www.soccerstats.com/widetable.asp?league=italy');
 });
 
 module.exports = app;
