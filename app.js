86fab54c96c505768502b225ff0456596cef115a
Sean Prasertsit <sean@prasertsit.com>
2015-04-23 17:46:55 -0700

    Updating added.

--- app.js
+++ app.js
@@ -3,12 +3,16 @@
  */
 
 var scraper = require('./lib/scrapers/clubScraper');
+//var updater = require('./lib/scrapers/clubUpdater');
 
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    scraper.scrape('http://www.soccerstats.com/widetable.asp?league=england');
+    scraper.scrape('http://www.soccerstats.com/widetable.asp?league=england', 'england1');
+    scraper.scrape('http://www.soccerstats.com/widetable.asp?league=spain', 'spain1');
+    scraper.scrape('http://www.soccerstats.com/widetable.asp?league=germany', 'germany1');
+    scraper.scrape('http://www.soccerstats.com/widetable.asp?league=italy', 'italy1');
 });
 
 module.exports = app;
