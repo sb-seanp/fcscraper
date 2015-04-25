b7aae7f1bdb99879b91714c714b1168af7e5a007
Sean Prasertsit <sean@prasertsit.com>
2015-04-25 14:49:59 -0700

    Final changes before production.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
  */
 
 var scraper = require('./lib/scrapers/clubScraper');
-//var updater = require('./lib/scrapers/clubUpdater');
 
 var express = require('express');
 var app = express();
@@ -13,6 +12,8 @@ app.get('/', function (req, res) {
     scraper.scrape('http://www.soccerstats.com/widetable.asp?league=spain', 'spain1');
     scraper.scrape('http://www.soccerstats.com/widetable.asp?league=germany', 'germany1');
     scraper.scrape('http://www.soccerstats.com/widetable.asp?league=italy', 'italy1');
+    scraper.scrape('http://www.soccerstats.com/widetable.asp?league=france', 'france1');
+    scraper.scrape('http://www.soccerstats.com/widetable.asp?league=portugal', 'portugal1');
 });
 
 module.exports = app;
